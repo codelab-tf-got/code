24710f570b90b4033b9ae4f0030268e7c84106ac
Santiago Saavedra <santiagosaavedra@gmail.com>
2017-05-21 18:03:47 +0200

    Update launch to work with updated requirements.txt and TF 1.1

--- launch.sh
+++ launch.sh
@@ -3,7 +3,6 @@
 virtualenv env
 source env/bin/activate
 pip install --upgrade pip
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl
 pip install -r requirements.txt
 
 cd wideep
